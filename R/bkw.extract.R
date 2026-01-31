7f00e885341a302275178715f18bbfc679b4dac6
Rui Peres dos Santos <rpsantos@pattern.institute>
2026-01-31 01:38:39 +0000

    Update `bkw.extract` for study dates and sst

--- R/bkw.extract.R
+++ R/bkw.extract.R
@@ -1,16 +1,41 @@
-#
-# Functions to extract the values of each day from each spatraster for all the variables
-#
+#' Generate study date sequence
+#'
+#' @description
+#'
+#' [study_dates()] generates a daily sequence of dates between the
+#' specified start and end dates. This sequence is used to index
+#' environmental raster time series.
+#'
+#' @param from A character or `Date` object indicating the start date.
+#'   Defaults to `"2012-01-01"`.
+#' @param to A character or `Date` object indicating the end date.
+#'   Defaults to `"2018-12-31"`.
+#'
+#' @returns A vector of class `Date` with daily resolution.
+#'
+#' @export
 study_dates <- function(from = "2012-01-01", to = "2018-12-31") {
   seq.Date(from = as.Date(from),
            to = as.Date(to),
            by = "day")
 }
 
-#
-# For sst variable
-#
-
+#' Index SST raster layers by date
+#'
+#' @description
+#'
+#' [sst_dates()] creates a lookup table linking each study date to
+#' the corresponding layer index in the SST NetCDF file.
+#'
+#' @returns A tibble with columns:
+#' \describe{
+#'   \item{date}{Daily study dates.}
+#'   \item{day}{Row index of the date sequence.}
+#'   \item{file}{Name of the NetCDF file containing SST data.}
+#'   \item{i}{Layer index within the NetCDF file corresponding to each date.}
+#' }
+#'
+#' @export
 sst_dates <- function() {
   tibble::tibble(date = study_dates()) |>
     tibble::rowid_to_column(var = "day") |>
@@ -20,6 +45,27 @@ sst_dates <- function() {
     dplyr::mutate(i = seq_len(dplyr::n()), .after = "date") |>
     dplyr::ungroup()
 }
+
+#' Read daily SST raster
+#'
+#' @description
+#'
+#' [read_sst()] reads the Sea Surface Temperature (SST) raster
+#' corresponding to a given date from a NetCDF file stored on disk.
+#' The raster is optionally smoothed using a focal mean and projected
+#' to the Azores analysis grid.
+#'
+#' SST values are converted from Kelvin to degrees Celsius.
+#'
+#' @param date A `Date` or character coercible to `Date`.
+#' @param dir Directory containing the SST NetCDF file.
+#' @param resolution Resolution (in meters) of the output raster grid.
+#'   Defaults to 500.
+#' @param w Window size used for focal smoothing. Defaults to 5.
+#'
+#' @returns A `SpatRaster` object projected to the Azores grid.
+#'
+#' @export
 read_sst <- function(date, dir, resolution = 500, w = 5) {
 
   sst_dates <- sst_dates()
@@ -38,6 +84,25 @@ read_sst <- function(date, dir, resolution = 500, w = 5) {
   terra::project(sp, y = azores.bathymetry::azores_grid(resolution = resolution, type = "rast"), method = "near")
 }
 
+#' Extract SST values at sf locations
+#'
+#' @description
+#'
+#' [extract_sst()] extracts Sea Surface Temperature (SST) values
+#' for each geometry in an `sf` object based on the corresponding
+#' `date` column.
+#'
+#' Extraction is performed by grouping records by date and matching
+#' each group to the appropriate raster layer.
+#'
+#' @param sf An `sf` object containing a `date` column.
+#' @param dir Directory containing the SST NetCDF file.
+#'
+#' @returns An `sf` object with the same columns as the input,
+#'   plus an additional column named `"sst"` placed before
+#'   the geometry column.
+#'
+#' @export
 extract_sst <- function(sf, dir) {
 
   sf_by_group <- dplyr::group_split(sf, .data$date)
